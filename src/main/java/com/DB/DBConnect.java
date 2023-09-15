b8b4b722dcb74eef3cc10605c50c67dab84d1c94
Romaylo-Watson <135418359+Romaylo-Watson@users.noreply.github.com>
2023-09-15 13:52:06 +0530

    update

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -1,41 +1,40 @@
-//package com.DB;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//
-//public class DBConnect {
-//	
-//	private static Connection conn;
-//	  public static Connection getConn() 
-//	  {
-//		
-//		  
-//		  try {
-//			  
-//			  if(conn==null) 
-//			  {
-//				 
-//				  Class.forName("com.mysql.cj.jdbc.Driver");
-//<<<<<<< HEAD
-//				  conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal-1","root","password");
-//				 
-//=======
-//				  conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal-1");
-//				  create statement = data connectiomn
-//>>>>>>> f0fd1ae0cfe2447fcf49732ad0a9b1fd95cd8e9b
-//			  }
-//			  
-//			  
-//			  
-//		  }catch(Exception e) {
-//			  e.printStackTrace();
-//		  }
-//		  
-//		
-//		  
-//		  
-//		  return conn;
-//		  
-//	  }
-//
-//}
+package com.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	
+	private static Connection conn;
+	  public static Connection getConn() 
+	  {
+		
+		  
+		  try {
+			  
+			  if(conn==null) 
+			  {
+				 
+				  Class.forName("com.mysql.cj.jdbc.Driver");
+
+				  conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal-1","root","password");
+				 
+
+				  conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal-1");
+				  
+			  }
+			  
+			  
+			  
+		  }catch(Exception e) {
+			  e.printStackTrace();
+		  }
+		  
+		
+		  
+		  
+		  return conn;
+		  
+	  }
+
+}
